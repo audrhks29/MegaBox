59a315beaeeed79a0d65eda33ac87ba8375ab484
audrhks29 <audrhks29@gmail.com>
2023-06-22 22:29:09 +0900

    MegaBox

--- src/components/MainArticle.jsx
+++ src/components/MainArticle.jsx
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { useRef } from 'react';
 import { useState } from 'react';
 import { useEffect } from 'react';
 import { AiOutlineHeart, AiFillHeart } from 'react-icons/ai';
@@ -9,11 +10,26 @@ const MainArticle = () => {
     const [text, setText] = useState('')
     const [search, setSearch] = useState('')
     const [filteredMovieData, setFilteredMovieData] = useState([])
+    const bgRef = useRef(null);
+    const articleRef = useRef(null);
+    const [articleHeight, setArticleHeight] = useState(0);
+    useEffect(() => {
+        const articleElement = articleRef.current;
+        if (articleElement) {
+            const { height } = articleElement.getBoundingClientRect();
+            setArticleHeight(height);
+        }
+    }, []);
+    const bgStyle = {
+        height: articleHeight
+    };
+    console.log(articleHeight);
     useEffect(() => {
         const url = `https://gist.githubusercontent.com/audrhks29/8d111972680ab3319c173096829c7504/raw/b7fb9dcbe3ddb9754bba0980707253c809180823/movie.json`
         axios.get(url)
             .then(res => {
                 setMovieData(res.data)
+                setFilteredMovieData(res.data)
                 setLoading(true)
                 setError(null)
                 console.log(res.data);
@@ -25,24 +41,58 @@ const MainArticle = () => {
             })
     }, [])
     const handleLikeToggle = (index) => {
-        setMovieData(prevData => {
+        setFilteredMovieData(prevData => {
             const newData = [...prevData];
-            newData[index] = { ...newData[index], isLike: !newData[index].isLike };
+            const updatedItem = { ...newData[index] };
+            updatedItem.isLike = !updatedItem.isLike;
+            updatedItem.audiCnt = updatedItem.isLike ? updatedItem.audiCnt + 1 : updatedItem.audiCnt - 1;
+            newData[index] = updatedItem;
             return newData;
         });
     };
-    const filteredData = movieData.filter(item => item.movieNm.includes(search))
+    // const onChange = (e) => {
+    //     setSearch(e.target.value)
+    //     setFilteredMovieData(movieData.filter(item => item.movieNm.includes(search)))
+    //     console.log(e.target.value);
+    // }
     const onChange = (e) => {
-        setSearch(e.target.value)
+        const inputValue = e.target.value;
+        setSearch(inputValue);
+        setFilteredMovieData(movieData.filter(item => item.movieNm.includes(inputValue)));
+    };
+    const showAllMovie = () => {
+        setFilteredMovieData(movieData)
+    }
+    const filmOnScreen = () => {
+        setFilteredMovieData(movieData.filter(item => item.screening === true))
+    }
+    const endOnScreen = () => {
+        setFilteredMovieData(movieData.filter(item => item.screening === false))
+    }
+    const numFormatter = (value) => {
+        if (value > 999 && value < 1000000) {
+            return (value / 1000).toFixed(1) + 'K';
+        } else if (value > 1000000) {
+            return (value / 1000000).toFixed(1) + 'M';
+        } else if (value < 900) {
+            return value;
+        }
     }
     return (
         <>
-            <div className="article">
+            <div className="bg" ref={bgRef} style={{ ...bgStyle, width: "100%" }}></div>
+            <div className="article" ref={articleRef}>
                 <h2>박스오피스</h2>
                 <input type="text" value={search} onChange={onChange} placeholder='검색어를 입력하세요' />
+                <div className='btnWrap'>
+                    <button onClick={showAllMovie}>전체</button>
+                    <button onClick={filmOnScreen}>상영중</button>
+                    <button onClick={endOnScreen}>상영종료</button>
+                    <h3>{filteredMovieData.length}개의 영화가 검색되었습니다.</h3>
+                </div>
                 <div className="BoxOffice" >
                     {
-                        filteredData.map((item, index) => {
+                        filteredMovieData.map((item, index) => {
                             const { imageURL, agelimit, movieNm, infoTitle, info, salesShare, openDt, audiCnt, isLike } = item;
                             return (
                                 <div key={index} className="BoxOffice-item">
@@ -66,7 +116,7 @@ const MainArticle = () => {
                                         <span><strong>개봉일 </strong>{openDt}</span>
                                     </div>
                                     <div className='like'>
-                                        <button onClick={() => handleLikeToggle(index)}><i>{isLike ? <AiFillHeart /> : <AiOutlineHeart />}<span>{audiCnt}</span></i></button>
+                                        <button onClick={() => handleLikeToggle(index)}><i>{isLike ? <AiFillHeart /> : <AiOutlineHeart />}<span>{numFormatter(audiCnt)}</span></i></button>
                                         <button><span>예매하기</span></button>
                                     </div>
                                 </div>
