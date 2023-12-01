86a7a674beab388ad60df253101da635fd6e923f
audrhks29 <audrhks29@gmail.com>
2023-12-01 18:04:52 +0900

    Update v1.2.3

--- src/components/BoxOffice.jsx
+++ src/components/BoxOffice.jsx
@@ -1,85 +1,100 @@
 
+import { useLayoutEffect, useState } from 'react';
+
 import { useDispatch, useSelector } from 'react-redux';
+
 import { getMovieData, toggleLike } from '../store/modules/movieSlice';
-import { useLayoutEffect } from 'react';
-import { AiOutlineHeart, AiFillHeart } from 'react-icons/ai';
 import { isPopupOpen } from '../store/modules/stateSlice';
+
+import { AiOutlineHeart, AiFillHeart } from 'react-icons/ai';
+
 import { BoxOfficeContainer, BoxOfficeItem } from '../styled/BoxOfficeStyle';
+
 const BoxOffice = () => {
-    const { pagingData, movieData, loading } = useSelector(state => state.movieR);
-    const dispatch = useDispatch()
+  const { pagingData, movieData, loading } = useSelector(state => state.movieR);
+  const dispatch = useDispatch()
+  const [onLoaded, setOnLoaded] = useState(false)
 
-    useLayoutEffect(() => {
-        if (loading) dispatch(getMovieData())
-    }, [movieData]);
+  useLayoutEffect(() => {
+    if (loading) dispatch(getMovieData())
+  }, [movieData]);
 
-    const handlePopup = (index) => {
-        dispatch(isPopupOpen(index))
+  const handlePopup = (index) => {
+    dispatch(isPopupOpen(index))
+  }
+
+  const handleLikeToggle = (index) => {
+    dispatch(toggleLike({ index }));
+  };
+
+  const numFormatter = (value) => {
+    if (value > 999 && value < 1000000) {
+      return (value / 1000).toFixed(1) + 'K';
+    } else if (value > 1000000) {
+      return (value / 1000000).toFixed(1) + 'M';
+    } else if (value < 900) {
+      return value;
     }
+  }
 
-    const handleLikeToggle = (index) => {
-        dispatch(toggleLike({ index }));
-    };
+  const handleOnLoad = () => {
+    setOnLoaded(true);
+  }
 
-    const numFormatter = (value) => {
-        if (value > 999 && value < 1000000) {
-            return (value / 1000).toFixed(1) + 'K';
-        } else if (value > 1000000) {
-            return (value / 1000000).toFixed(1) + 'M';
-        } else if (value < 900) {
-            return value;
-        }
-    }
-
-    return (
-        <BoxOfficeContainer>
-            {
-                pagingData.map((item, index) => {
-                    return (
-                        <BoxOfficeItem key={index}>
-                            <div className="BoxOffice-img">
-                                <img src={item.imageURL} style={{ width: 245, height: 350 }} alt="" />
-                            </div>
-                            <div className="BoxOffice-name">
-                                {item.agelimit === 12 && <img src="../public/images/age/12.png" alt="" />}
-                                {item.agelimit === 15 && <img src="../public/images/age/15.png" alt="" />}
-                                {item.agelimit === 18 && <img src="../public/images/age/18.png" alt="" />}
-                                {item.agelimit === 0 && <img src="../public/images/age/all.png" alt="" />}
-                                <span >{item.movieNm}</span>
-                            </div>
-                            <div className="BoxOffice-info" onClick={() => handlePopup(index)}>
-                                <span>{item.infoTitle}</span>
-                                {
-                                    item.info && item.info.split('\n').map((splitItem, index) => {
-                                        return <span key={index}>{splitItem}</span>
-                                    })
-                                }
-                            </div>
-                            <div className='BoxOffice-per_date'>
-                                <span><strong>예매율 </strong>{item.salesShare}%</span>
-                                <span><strong>개봉일 </strong>{item.openDt}</span>
-                            </div>
-                            <div className='like' >
-                                <button
-                                    onClick={() => handleLikeToggle(index)}
-                                >
-                                    <i>
-                                        {
-                                            item.isLike
-                                                ? <AiFillHeart />
-                                                : <AiOutlineHeart />
-                                        }
-                                        <span>{numFormatter(item.audiCnt)}</span>
-                                    </i>
-                                </button>
-                                <button><span>예매하기</span></button>
-                            </div >
-                        </BoxOfficeItem>
-                    )
-                })
-            }
-        </BoxOfficeContainer>
-    );
+  return (
+    <BoxOfficeContainer>
+      {
+        pagingData.map((item, index) => {
+          return (
+            <BoxOfficeItem key={index}>
+              <div className="BoxOffice-img">
+                <img
+                  src={item.imageURL}
+                  style={{ display: onLoaded ? 'block' : "none", width: 245, height: 350 }}
+                  alt=""
+                  onLoad={handleOnLoad}
+                />
+              </div>
+              <div className="BoxOffice-name">
+                {item.agelimit === 12 && <img src="../public/images/age/12.png" alt="" />}
+                {item.agelimit === 15 && <img src="../public/images/age/15.png" alt="" />}
+                {item.agelimit === 18 && <img src="../public/images/age/18.png" alt="" />}
+                {item.agelimit === 0 && <img src="../public/images/age/all.png" alt="" />}
+                <span >{item.movieNm}</span>
+              </div>
+              <div className="BoxOffice-info" onClick={() => handlePopup(index)}>
+                <span>{item.infoTitle}</span>
+                {
+                  item.info && item.info.split('\n').map((splitItem, index) => {
+                    return <span key={index}>{splitItem}</span>
+                  })
+                }
+              </div>
+              <div className='BoxOffice-per_date'>
+                <span><strong>예매율 </strong>{item.salesShare}%</span>
+                <span><strong>개봉일 </strong>{item.openDt}</span>
+              </div>
+              <div className='like' >
+                <button
+                  onClick={() => handleLikeToggle(index)}
+                >
+                  <i>
+                    {
+                      item.isLike
+                        ? <AiFillHeart />
+                        : <AiOutlineHeart />
+                    }
+                    <span>{numFormatter(item.audiCnt)}</span>
+                  </i>
+                </button>
+                <button><span>예매하기</span></button>
+              </div >
+            </BoxOfficeItem>
+          )
+        })
+      }
+    </BoxOfficeContainer>
+  );
 };
 
 export default BoxOffice;
